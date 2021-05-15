abaf217db17f090f56bb8b7a0044b46333d5191e
Naomi Hidalgo <nhid@elpuig.xeill.net>
2021-05-15 20:45:43 +0200

    puyesto models y se instala

--- models/models.py
+++ models/models.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class recetas(models.Model):
-#     _name = 'recetas.recetas'
+class recetas(models.Model):
+    _name = 'recetas.recetas'
 #     _description = 'recetas.recetas'
 
-#     name = fields.Char()
-#     value = fields.Integer()
+    name = fields.Char()
+    id = fields.Char()
+    dificultad = fields.Char()
+
+    time = fields.Integer()
+    
 #     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+    description = fields.Text()
+    steps = fields.Text()
+
+    vegano = fields.Boolean()
+    celiaco = fields.Boolean()
+    
+    dateAdded = fields.Date()
+    #Imagen?
+
 #
 #     @api.depends('value')
 #     def _value_pc(self):
